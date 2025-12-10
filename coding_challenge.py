efb3e3f0b99a2ee9fea741c66aaa506c0b595507
Aishwarya C <aishwaryachandalada@gmail.com>
2025-12-10 20:27:12 +0000

    challenge 1

---
+++ coding_challenge.py
@@ -0,0 +1,12 @@
+#Challenge one
+
+import math
+
+hours_worked = int(input("Enter a Number for the Amount of Hours Worked: "))
+hours_slept = 24 - hours_worked
+print(hours_slept)
+
+#Challenge 2
+
+players = int(input("Enter a number of players"))
+matches = int(input("Enter a number of matches played: "))
